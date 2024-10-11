228ce792e86ac7119a97deefc9aa95c6daec7359
paula <paulayaqueline.benitez@gracia.lasalle.cat>
2024-10-11 10:55:09 +0200

    commitFinal
    por favor

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("La Lucía es la mejor, no hay discusión.");
+        System.out.println("Hello world!");
+        System.out.println("Cambio weiafherp9fhewpf9");
+        System.out.println("lo logramos! (odiamos github)");
     }
 }
